1182590e129454669b2f543f2fe5ef138535d1da
thangtm <thangtruong.dev@gmail.com>
2018-12-24 09:15:53 +0700

    Initial Commit

---
+++ ManToMan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManToMan
+//
+//  Created by Thang Hoa on 12/24/18.
+//  Copyright © 2018 Thang Hoa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
